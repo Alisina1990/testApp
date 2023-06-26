ef14a692a864785e08fc8456b2524fcc3c4ecb56
Alisina1990 <alisinamohammadnabi1990@gmail.com>
2023-06-26 15:58:50 +0200

    Now we have many masseges.

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Helloooo, C# Academy more modification");
+Console.WriteLine("Helloooo, C# Academy more modification");
+Console.WriteLine("Helloooo, C# Academy more modification");
+Console.WriteLine("Helloooo, C# Academy more modification");
+Console.WriteLine("Helloooo, C# Academy more modification");
