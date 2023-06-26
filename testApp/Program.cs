7e2a64a94c6c1b058feda6be798f57ed624f8773
Alisina <76634191+Alisina1990@users.noreply.github.com>
2023-06-26 16:02:37 +0200

    Update Program.cs

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Helloooo, C# Academy more modification");
-Console.WriteLine("Helloooo, C# Academy more modification");
-Console.WriteLine("Helloooo, C# Academy more modification");
-Console.WriteLine("Helloooo, C# Academy more modification");
-Console.WriteLine("Helloooo, C# Academy more modification");
+
